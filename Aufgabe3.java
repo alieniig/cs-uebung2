3308808951d8c7a9d6d6e44f3467bbf8b75be5c2
alieniig <s_lienig22@hwr-berlin.de>
2024-03-13 11:01:15 +0100

    Aufgabe 3 fertig

--- Aufgabe3.java
+++ Aufgabe3.java
@@ -7,20 +7,10 @@ import java.util.Scanner;
 public class Aufgabe3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-<<<<<<< HEAD
-    
+
         System.out.println("Hostname:");
         String domainName = scanner.nextLine();
   
-=======
-        
-        System.out.println("Hostname:");
-        String domainName = scanner.nextLine();
-        if(scanner.nextLine()=="quit"){
-            scanner.close();
-        }
-
->>>>>>> test-repo/main
         try {
             // Verbindung aufbauen
             Socket socket = new Socket(domainName, 13);
